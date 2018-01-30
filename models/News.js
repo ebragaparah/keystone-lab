5e4179f13d7c03d21c06c1a22b805c1a34ac7c23
Eduardo <ebfjunior@gmail.com>
2018-01-30 16:54:39 -0200

    nova estrutura de modelo news

--- models/News.js
+++ models/News.js
@@ -7,14 +7,37 @@ var News = new keystone.List('News', {
 });
 
 News.add({
-  category: { type: String },
-  hat: { type: String },
-  title: { type: String },
-  author: { type: String },
-  description: {
-    brief: { type: Types.Html, wysiwyg: true, height: 150 },
-    extended: { type: Types.Html, wysiwyg: true, height: 400 }
-  }
+  _id: { type: String },
+  updated_at: { type: Date, default: Date.now },
+  published_at: { type: Date, default: Date.now },
+  created_at: { type: Date, default: Date.now },
+  status: { type: String },
+  sections: ,
+  tags: ,
+  related_news: { type: Types.Relationship, ref: 'News', many: true },
+  metadata: {
+    layout: { type: String },
+    title: { type: String },
+    cover: {
+      link: { type: String },
+      original: { type: String },
+      thumbnail: { type: String },
+      medium: { type: String },
+      small: { type: String },
+      title: { type: String },
+      credits: { type: String },
+      subtitle: { type: String },
+      mobile : {
+	small: { type: String },
+	medium: { type: String },
+	thumbnail: { type: String },
+	original: { type: String },
+	link: { type: String }
+      }
+    },
+    url: { type: String }
+  },
+  __v: { type: String }
 });
 
 News.register();
